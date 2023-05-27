0cec6a1b0a58539709e67fe8fadb374007b7f7be
sk shadik <57946665+SK-SHADIK@users.noreply.github.com>
2023-05-27 12:43:47 +0600

    Lender Search Sub Table Migration

---
+++ database/migrations/2023_05_27_061713_create_immig_status_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('immig_status', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->boolean('is_active')->default(true);
+            $table->string('cb', 255)->nullable();
+            $table->timestamp('cd')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->string('ub', 255)->nullable();
+            $table->timestamp('ud')->default(DB::raw('CURRENT_TIMESTAMP'));
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('immig_status');
+    }
+};
