c148a49759c40fab38fbaf6fe446e3de819f67a5
sk shadik <57946665+SK-SHADIK@users.noreply.github.com>
2023-07-28 21:25:03 +0600

    In Login Log Try Catch

--- app/Http/Controllers/Login.php
+++ app/Http/Controllers/Login.php
@@ -8,71 +8,78 @@ use DateInterval;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Token;
-use App\Models\LoanOfficerModel;
-use App\Models\ClientsModel;
 use DB;
+use Illuminate\Support\Facades\Log;
+use Exception;
+use Illuminate\Database\QueryException;
 
 class Login extends Controller
 {
-    //
     public function login(Request $request)
     {
+        try {
+            $validator = Validator::make(
+                $request->all(),
+                [
+                    'email' => "required",
+                    'password' => "required",
+                ],
+                [
+                    "email.required" => "Please provide your email!!!",
+                    "password.required" => "Please provide your password!!!",
+                ]
+            );
 
-        $validator = Validator::make(
-            $request->all(),
-            [
-                'email' => "required",
-                'password' => "required",
-            ],
-            [
-                "email.required" => "Please provide your email!!!",
-                "password.required" => "Please provide your password!!!",
-            ]
-        );
+            $email = $request['email'];
 
-        $email = $request['email'];
-
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 422);
-        }
-
-        $user1 = DB::select("SELECT * FROM loan_officer WHERE email = '$email' LIMIT 1;");
-        $user2 = DB::select("SELECT * FROM clients WHERE email = '$email' LIMIT 1;");
-
-        $key = Str::random(40);
-        $token = new Token();
-
-        if ($user1) {
-            $user1 = $user1[0];
-            if (md5($request->password) == $user1->password) {
-
-                $token->token_key = $key;
-                $token->email = $user1->email;
-                $token->created_at = new Datetime();
-                $token->expired_at = (new DateTime())->add(new DateInterval('P1D'));
-                $token->save();
-                return response()->json(["token" => $key, "user" => "Loan Officer"], 200);
-
-            } else {
-                return response()->json(["msg" => "Invalid Username or Password!!!"], 422);
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 422);
             }
 
-        } else if ($user2) {
-            $user2 = $user2[0];
-            if (md5($request->password) == $user2->password) {
+            $user1 = DB::select("SELECT * FROM loan_officer WHERE email = '$email' LIMIT 1;");
+            $user2 = DB::select("SELECT * FROM clients WHERE email = '$email' LIMIT 1;");
 
-                $token->token_key = $key;
-                $token->email = $user2->email;
-                $token->created_at = new Datetime();
-                $token->expired_at = (new DateTime())->add(new DateInterval('P1D'));
-                $token->save();
-                return response()->json(["token" => $key, "user" => "Client"], 200);
+            $key = Str::random(40);
+            $token = new Token();
 
+            if ($user1) {
+                $user1 = $user1[0];
+                if (md5($request->password) == $user1->password) {
+
+                    $token->token_key = $key;
+                    $token->email = $user1->email;
+                    $token->created_at = new Datetime();
+                    $token->expired_at = (new DateTime())->add(new DateInterval('P1D'));
+                    $token->save();
+                    return response()->json(["token" => $key, "user" => "Loan Officer"], 200);
+
+                } else {
+                    return response()->json(["msg" => "Invalid Username or Password!!!"], 422);
+                }
+
+            } else if ($user2) {
+                $user2 = $user2[0];
+                if (md5($request->password) == $user2->password) {
+
+                    $token->token_key = $key;
+                    $token->email = $user2->email;
+                    $token->created_at = new Datetime();
+                    $token->expired_at = (new DateTime())->add(new DateInterval('P1D'));
+                    $token->save();
+                    return response()->json(["token" => $key, "user" => "Client"], 200);
+
+                } else {
+                    return response()->json(["msg" => "Invalid Username or Password!!!"], 422);
+                }
             } else {
-                return response()->json(["msg" => "Invalid Username or Password!!!"], 422);
+                return response()->json(["msg" => "This Email Is Not Registered!!!"], 422);
             }
-        } else {
-            return response()->json(["msg" => "This Email Is Not Registered!!!"], 422);
+        } catch (QueryException $e) {
+            Log::error(__FILE__ . ' || Line ' . __LINE__ . ' || ' . $ex->getMessage() . ' || ' . $ex->getCode());
+            return response()->json(["error" => "Database Error Occurred!!! Please Try Again."], 500);
+        } catch (Exception $ex) {
+            Log::error(__FILE__ . ' || Line ' . __LINE__ . ' || ' . $ex->getMessage() . ' || ' . $ex->getCode());
+            return response()->json(["error" => "An Unexpected Error Occurred!!! Please Try Again."], 500);
         }
 
     }
